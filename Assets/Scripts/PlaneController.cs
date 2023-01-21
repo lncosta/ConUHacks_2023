6c93a4bf9ff98f92b519ecc033eb6419c1d953c2
Luiza Costa <luiza.n16@gmail.com>
2023-01-21 12:02:02 -0500

    Initial scene

---
+++ Assets/Scripts/PlaneController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] public GameObject plane;
+    [SerializeField] public float roll;
+    [SerializeField] public float pitch;
+    [SerializeField] public float yaw;
+    [SerializeField] public float throttle;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
